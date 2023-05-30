e91e5076f8eb30ec396de530664fa38f07a523e3
Jagmeister <unsaid-09.imagers@icloud.com>
2023-05-30 12:30:55 -0400

    Initial Commit

---
+++ busscard/busscardApp.swift
@@ -0,0 +1,17 @@
+//
+//  busscardApp.swift
+//  busscard
+//
+//  Created by Javier Rendon on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct busscardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
